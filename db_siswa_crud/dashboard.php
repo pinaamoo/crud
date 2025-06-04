ed1a27b9472e18923275fb86bd9d034452fd37ef
pinaamoo <magfirotunanifa11@gmail.com>
2025-06-04 18:37:31 +0700

    Delete db_siswa_crud directory

--- db_siswa_crud/dashboard.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['username'])) {
-    header("location: login.php");
-    exit();
-}
-
-echo "Selamat datang, " . $_SESSION['username'] . " (" . $_SESSION['role'] . ")";
-?>
